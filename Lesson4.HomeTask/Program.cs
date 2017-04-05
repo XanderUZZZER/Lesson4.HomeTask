1d8fb4bb50b7780240fab064acfd0cb737f15fc2
Xander_UZZZER <Xander_UZZZER@XANDER-W10-O>
2017-04-05 11:33:31 +0300

    0504171133

--- Lesson4.HomeTask/Program.cs
+++ Lesson4.HomeTask/Program.cs
@@ -10,6 +10,26 @@ namespace Lesson4.HomeTask
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Days to birthday");
+            Console.Write("Input your birthday date in format dd/mm/yyyy: ");
+
+            DateTime bithrday = DateTime.Parse(Console.ReadLine());
+            DateTime today = DateTime.Now;
+            DateTime nextBirtday;
+
+            if (today.DayOfYear < bithrday.DayOfYear)
+            {
+                nextBirtday = new DateTime(today.Year, bithrday.Month, bithrday.Day);
+            }
+            else
+            {
+                nextBirtday = new DateTime(today.Year + 1, bithrday.Month, bithrday.Day);
+            }
+
+            TimeSpan daysLeft = nextBirtday.Subtract(today);
+
+            Console.WriteLine((int)daysLeft.TotalDays + " days left to next birthday.");
+            Console.ReadLine();
         }
     }
 }
